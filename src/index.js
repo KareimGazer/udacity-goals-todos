6a87991754634fc8e1626133239e2525a553556d
Kareim Tarek AbdelAzeem <kareimgazar1990@gmail.com>
2021-08-27 00:16:57 +0200

    </Provider>,

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import reportWebVitals from "./reportWebVitals";
 
+const store = createStore(reducer, middleware);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
